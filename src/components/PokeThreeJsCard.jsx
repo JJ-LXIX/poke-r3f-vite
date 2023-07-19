55f178e32c9ced5f6af633dc009e8821c03b33d2
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-19 19:35:34 +0530

    added state,ref and zoom animation

--- src/components/PokeThreeJsCard.jsx
+++ src/components/PokeThreeJsCard.jsx
@@ -8,6 +8,28 @@ import React, { useEffect, useRef, useState } from "react";
 import { useThree } from "@react-three/fiber";
 
 function PokeThreeJs({ active, setActive }) {
+  const [hovered, setHovered] = useState();
+  const controlsRef = useRef();
+  const scene = useThree((state) => state.scene);
+  useCursor(hovered);
+
+  useEffect(() => {
+    if (active) {
+      const targetPosition = new THREE.Vector3();
+      scene.getObjectByName(active)?.getWorldPosition(targetPosition);
+      controlsRef.current.setLookAt(
+        0,
+        0,
+        4,
+        targetPosition.x,
+        targetPosition.y,
+        targetPosition.z,
+        true
+      );
+    } else {
+      controlsRef.current.setLookAt(0, 0, 10, 0, 0, 0, true);
+    }
+  }, [active]);
   return (
     <>
     </>
