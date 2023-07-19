8f3fdfb181724c827e534ed6e1e83c1b5aef4e61
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-19 19:34:25 +0530

    added PokeThreeJsCard.jsx

---
+++ src/components/PokeThreeJsCard.jsx
@@ -0,0 +1,17 @@
+import { CameraControls, Environment, useCursor } from "@react-three/drei";
+import Squirtle2 from "/src/components/Squirtle2";
+import Bulbasaur2 from "/src/components/Bulbasaur2";
+import Charmander2 from "/src/components/Charmander2";
+import PokeStage from "./PokeStage";
+import * as THREE from "three";
+import React, { useEffect, useRef, useState } from "react";
+import { useThree } from "@react-three/fiber";
+
+function PokeThreeJs({ active, setActive }) {
+  return (
+    <>
+    </>
+  );
+}
+
+export default PokeThreeJs;
